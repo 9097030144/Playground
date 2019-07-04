e0da60882eef8d7532974e26578ed30ffd838256
9097030144 <32329580+9097030144@users.noreply.github.com>
2019-07-04 23:46:43 +0530

    Test code pushed to repository.

---
+++ Multiply two numbers/Main.java
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  int p = 63, q = 81, r;
+	r = p * q;
+	printf("%d", r);
+	
+  return 0;
+}
---
+++ Sum of three numbers/Main.java
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  int p = 27, q = 65, r = 79, s;
+	s = p + q + r;
+	printf("%d", s);
+	
+  return 0;
+}
